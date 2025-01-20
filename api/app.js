422e69c264b99a00cd239e7b4f86774a37ce6e9c
user <1001inchnails@gmail.com>
2025-01-20 16:56:52 +0100

    lll

--- api/app.js
+++ api/app.js
@@ -1,18 +1,9 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
 
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const api = require('./api');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -60,9 +51,5 @@ app.get('/users/:id',(req, res)=>{
   }
 });
 
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 module.exports = app;
